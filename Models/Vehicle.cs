a95d27f0b7712d27c2ad3b9d2f3ebc334c6324c6
ZEMDESKTOP\Felipe Lopes Zem <felipelopeszem@hotmail.com>
2025-06-11 23:53:47 -0300

    Revert "WIP - setting which fields can be edited or not"
    This reverts commit e2f779ec2a1b2504d6b818083ff1b9a63a8b8a74.

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,39 +1,30 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using System.ComponentModel;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using VolvoWebApp.Enums;
 
 namespace VolvoWebApp.Models
 {
     public class Vehicle
     {
-        [Required]
         [DisplayName("Id")]
-        [Editable(false)]
         public string Id { get; set; } = $"{Guid.NewGuid()}";
-        [Required]
         [DisplayName("Chassis Series")]
-        [Editable(false)]
         public string ChassisSeries { get; set; }
-        [Required]
         [DisplayName("Chassis Number")]
-        [Editable(false)]
         public uint ChassisNumber { get; set; }
-        [Required]
         [DisplayName("Chassis Id")]
-        [Editable(false)]
         public string ChassisId => $"{ChassisSeries}/{ChassisNumber}";
-        [Required]
         [DisplayName("Type")]
-        [Editable(false)]
         public VehicleType Type { get; set; }
-        [Required]
         [DisplayName("Number of Passengers")]
-        [Editable(false)]
         public uint NumberOfPassengers => Type.NumberOfPassengers();
-        [Required]
         [DisplayName("Color")]
-        [Editable(true)]
-        public string Color { get; set; }
+        public string? Color { get; set; }
+
+        public Vehicle()
+        {
+            
+        }
     }
 }
