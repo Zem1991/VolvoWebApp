3aa50346125ebc0e0c02251a8d3bbc96d7a8d268
ZEMDESKTOP\Felipe Lopes Zem <felipelopeszem@hotmail.com>
2025-06-11 23:02:23 -0300

    Visual fixes.

--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
 using VolvoWebApp.Enums;
 
 namespace VolvoWebApp.Models
 {
     public class Vehicle
     {
+        [DisplayName("Id")]
         public string Id { get; set; }
+        [DisplayName("Chassis Series")]
         public string ChassisSeries { get; set; }
+        [DisplayName("Chassis Number")]
         public uint ChassisNumber { get; set; }
+        [DisplayName("Chassis Id")]
         public string ChassisId => $"{ChassisSeries}/{ChassisNumber}";
+        [DisplayName("Type")]
         public VehicleType Type { get; set; }
+        [DisplayName("Number of Passengers")]
         public uint NumberOfPassengers => Type.NumberOfPassengers();
+        [DisplayName("Color")]
         public string? Color { get; set; }
 
         public Vehicle()
